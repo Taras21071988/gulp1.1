0b425a56740ca0b44c034b85f0c9c351658aac45
Taras21071988 <tsasojlov@icloud.com>
2023-11-02 12:46:47 +0200

    creating a task for compiling and assembling styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require("gulp");
+const { require } = require("gulp-cli/lib/shared/cli-options");
 const fileInclude = require("gulp-file-include");
+const sass = require('gulp-sass')(require('sass'))
+
 
 const fileIncludeSetting = {
   prefix: "@@",
@@ -11,4 +14,10 @@ gulp.task("includeFiles", function () {
     .src("./src/*.html")
     .pipe(fileInclude(fileIncludeSetting))
     .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("sass", function () {
+  return gulp.src("./src/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css/'));
 });
