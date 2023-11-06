bc32115d7f110c3484eac31044cf8866f2242e94
Taras21071988 <tsasojlov@icloud.com>
2023-11-06 13:32:41 +0200

    creating a task to copy fonts and files

--- gulpfile.js
+++ gulpfile.js
@@ -28,10 +28,17 @@ const plumberSassConfig = {
   }),
 };
 
+const plumberHtmlConfig = {
+  errorHandler: notify.onError({
+    title: "Html",
+    message: "Error <%= error.message %>",
+    sound: false,
+  }),
+};
 gulp.task("html", function () {
   return gulp
     .src("./src/*.html")
-    .pipe(plumber(plumberSassConfig))
+    .pipe(plumber(plumberHtmlConfig))
     .pipe(fileInclude(fileIncludeSetting))
     .pipe(gulp.dest("./dist/"));
 });
@@ -52,6 +59,12 @@ gulp.task("sass", function () {
 gulp.task("images", function () {
   return gulp.src("./src/img/**/*").pipe(gulp.dest("./dist/img/"));
 });
+gulp.task("fonts", function () {
+  return gulp.src("./src/fonts/**/*").pipe(gulp.dest("./dist/fonts/"));
+});
+gulp.task("files", function () {
+  return gulp.src("./src/files/**/*").pipe(gulp.dest("./dist/files/"));
+});
 
 gulp.task("server", function () {
   return gulp.src("./dist/").pipe(server(startServerSetting));
@@ -68,13 +81,15 @@ gulp.task("watch", function () {
   gulp.watch("./src/scss/**/*.scss", gulp.parallel("sass"));
   gulp.watch("./src/**/*.html", gulp.parallel("html"));
   gulp.watch("./src/img/**/*", gulp.parallel("images"));
+  gulp.watch("./src/img/**/*", gulp.parallel("fonts"));
+  gulp.watch("./src/img/**/*", gulp.parallel("files"));
 });
 
 gulp.task(
   "default",
   gulp.series(
     "clean",
-    gulp.parallel("html", "sass", "images"),
+    gulp.parallel("html", "sass", "images", "fonts", "files"),
     gulp.parallel("server", "watch")
   )
 );
