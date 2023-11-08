6b868925ceb2cb18e81794c37b1443fb0dbf207d
Taras21071988 <tsasojlov@icloud.com>
2023-11-08 12:43:24 +0200

    connecting an npm package in js (example with connecting a calendar)

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 import hello from "./modules/hello";
 
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
+
+new AirDatepicker('#date', {
+    selectedDates: [new Date()]
+})
+
 console.log("Hello performance testing webpack", hello);
