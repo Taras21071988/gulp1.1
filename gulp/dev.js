fb5a60eb9b78699fdb232fb94a3f5fc97b86998d
Taras21071988 <tsasojlov@icloud.com>
2023-11-10 12:53:52 +0200

    editing the html task, namely the gulp-changed package

--- gulp/dev.js
+++ gulp/dev.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const fileInclude = require("gulp-file-include");
 const sass = require("gulp-sass")(require("sass"));
-const sassGlob = require('gulp-sass-glob')
+const sassGlob = require("gulp-sass-glob");
 const server = require("gulp-server-livereload");
 const clean = require("gulp-clean");
 const fs = require("fs");
@@ -12,9 +12,6 @@ const webpack = require("webpack-stream");
 const babel = require("gulp-babel");
 const imagemin = require("gulp-imagemin");
 const changed = require("gulp-changed");
-
-
-
 
 const fileIncludeSetting = {
   prefix: "@@",
@@ -40,24 +37,22 @@ const plumberNotify = (title) => {
 gulp.task("html:dev", function () {
   return gulp
     .src(["./src/html/**/*.html", "!./src/html/blocks/*.html"])
-    .pipe(changed("./build/"))
+    .pipe(changed("./build/", { hasChanged: changed.compareContents }))
     .pipe(plumber(plumberNotify("html")))
     .pipe(fileInclude(fileIncludeSetting))
     .pipe(gulp.dest("./build/"));
 });
 
 gulp.task("sass:dev", function () {
-  return (
-    gulp
-      .src("./src/scss/*.scss")
-      .pipe(changed("./build/css/"))
-      .pipe(plumber(plumberNotify("Styles")))
-      .pipe(sourceMaps.init())
-      .pipe(sassGlob())
-      .pipe(sass())
-      .pipe(sourceMaps.write())
-      .pipe(gulp.dest("./build/css/"))
-  );
+  return gulp
+    .src("./src/scss/*.scss")
+    .pipe(changed("./build/css/"))
+    .pipe(plumber(plumberNotify("Styles")))
+    .pipe(sourceMaps.init())
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest("./build/css/"));
 });
 
 gulp.task("images:dev", function () {
@@ -111,5 +106,3 @@ gulp.task("watch:dev", function () {
   gulp.watch("./src/files/**/*", gulp.parallel("files:dev"));
   gulp.watch("./src/js/**/*.js", gulp.parallel("js:dev"));
 });
-
-
